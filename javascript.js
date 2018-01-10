647c0550fb67f64a85109a7966873502595e63b4
jbrain98 <33076782+jbrain98@users.noreply.github.com>
2018-01-09 18:43:31 -0600

    Create javascript.js

---
+++ javascript.js
@@ -0,0 +1,18 @@
+var artistSelect = document.getElementById("artistSelect");
+
+
+var arts = document.getElementsByClassName("art");
+
+function findArtist(){
+  showAll();
+  for(var i = 0; i < arts.length; i++) {
+    if(!(arts[i].classList.contains(artistSelect.value) || artistSelect.value === "all")
+      arts[i].style.display="none";
+  }
+}
+
+function showAll() {
+  for (var i = 0; i < arts.length; i++) {
+    arts[i].style.display="block";
+  }
+}
